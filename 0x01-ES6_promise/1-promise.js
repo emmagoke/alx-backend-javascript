cc0d8becb9fc4600a9c42daaad872c8b9217a5a2
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-06-27 19:15:45 +0100

    Added task 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -3,7 +3,7 @@ export default function getFullResponseFromAPI(success) {
     if (success) {
       resolve({ status: 200, body: 'Success' });
     } else {
-      reject('The fake API is not working currently'); // eslint-disable-line
+      reject(new Error('The fake API is not working currently'));
     }
   });
 }

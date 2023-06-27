8bdc4a680d9de49a03c6a4ebb4559cec79442c65
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-06-27 21:43:10 +0100

    Added task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,5 +2,6 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then((valArray) => console.log(`${valArray[0].body} ${valArray[1].firstName} ${valArray[1].lastName}`));
+    .then((valArray) => console.log(`${valArray[0].body} ${valArray[1].firstName} ${valArray[1].lastName}`))
+    .catch(() => console.log('Signup system offline'));
 }

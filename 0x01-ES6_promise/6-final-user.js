04cac6c37d926edf5f0c486486da53dc4f13dcca
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-06-28 01:45:10 +0100

    Made some corrections

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,5 +3,16 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((result) => [{ status: result.status, value: result.value }]);
+    .then((result) => [
+      {
+        status: result.status,
+        value: result.status === 'fulfilled' ? result.value : String(result.reason),
+      }]);
 }
+
+// status
+//     A string, either "fulfilled" or "rejected", indicating the eventual state of the promise.
+// value
+//     Only present if status is "fulfilled". The value that the promise was fulfilled with.
+// reason
+//     Only present if status is "rejected". The reason that the promise was rejected with.

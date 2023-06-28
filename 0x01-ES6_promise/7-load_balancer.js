b4ca960fe541ccf5d145c1f420f85cecf3434736
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-06-28 02:24:28 +0100

    Added task 7

---
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -0,0 +1,5 @@
+// return the value returned by the promise that resolved the first.
+export default function loadBalancer(chinaDownload, USDownload) {
+  return Promise.race([chinaDownload, USDownload])
+    .then((value) => value);
+}

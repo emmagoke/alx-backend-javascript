5433cb0cafc3a0005d1cafe71f1de493b405bace
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-06-29 20:57:22 +0100

    Made corrections

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -7,9 +7,9 @@ export default class HolbertonCourse {
    * @param {String[]} students - The names of students in the course.
    */
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
   // getter for name

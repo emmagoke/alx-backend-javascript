57e79acfc3997ce8e239c8d9b7152b05470f7161
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-07-03 14:14:24 +0100

    Added question 1

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+/* This file contains getListStudents array that returns
+three object. */
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}

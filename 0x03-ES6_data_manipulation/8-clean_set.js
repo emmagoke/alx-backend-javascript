c94cf21ed89c2711b3fd5fd54fcc214bba067d5c
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-07-07 20:51:40 +0100

    Added task 5

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,17 @@
+/*
+This script contains a function that check if a set of string
+starts with a string
+*/
+export default function cleanSet(set, substring) {
+  // If substring is an empty string
+  if (!substring) {
+    return '';
+  }
+  const output = [];
+  for (const string of set) {
+    if (string.startsWith(substring)) {
+      output.push(string.substring(substring.length));
+    }
+  }
+  return output.join('-');
+}

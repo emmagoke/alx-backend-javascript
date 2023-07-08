eae8b32e88de436b194e3c74a027db75b0c9ef09
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-07-08 02:49:06 +0100

    Added task 4

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -4,11 +4,42 @@
 // location(string) this attribute should always be defined
 // Add the possibility to add any attribute to the Object like contract(boolean) without specifying the name of the attribute
 
-interface Teacher {
+export interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
   fullTimeEmployee: boolean;
   yearsOfExperience?: number;
   location: string;
   [index: string]: any;
+}
+
+// Write an interface named Directors that extends Teacher. It requires an attribute named numberOfReports(number)
+export interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+export interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+export function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName[0]}.  ${lastName}`;
+}
+
+export class StudentClass {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
 }

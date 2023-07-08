d0c179f2303892ac16a149556a176931c6613b89
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-07-08 01:45:07 +0100

    Added task 1

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,14 @@
+// firstName(string) and lastName(string). These two attributes should only be modifiable when a Teacher is first initialized
+// fullTimeEmployee(boolean) this attribute should always be defined
+// yearsOfExperience(number) this attribute is optional
+// location(string) this attribute should always be defined
+// Add the possibility to add any attribute to the Object like contract(boolean) without specifying the name of the attribute
+
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [index: string]: any;
+}

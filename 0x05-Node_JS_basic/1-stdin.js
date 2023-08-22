aecac967233e6b9048d44584713ccaaa6b392ed5
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-08-22 02:07:47 +0100

    Added question 1 solution

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,13 @@
 // This script contains a code that takes input from the user
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('readable', () => {
-  const input = process.stdin.read();
-  if (input !== null) {
-    process.stdout.write(`Your name is: ${input}`);
+process.stdin.on('data', (data) => {
+  if (data) {
+    process.stdout.write(`Your name is: ${data}`);
   }
+  process.exit();
+});
+
+process.on('exit', () => {
   process.stdout.write('This important software is now closing\n');
 });

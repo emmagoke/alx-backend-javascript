498bed17dc1fa0bc648223100d12c1fbf5210b6b
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-08-22 01:39:14 +0100

    Added question 1 solution

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,7 +3,7 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('readable', () => {
   const input = process.stdin.read();
   if (input !== null) {
-    console.log(`Your name is: ${input}`);
+    process.stdout.write(`Your name is: ${input.toString()}`);
   }
   console.log('This important software is now closing');
 });
